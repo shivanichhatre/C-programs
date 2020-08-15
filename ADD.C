ca04ff8aca6491360e6c510dd18411a4d7c69872
Shivani Chhatre <69643579+shivanichhatre@users.noreply.github.com>
2020-08-15 19:37:57 +0530

    Add files via upload
    Addition of two numbers in c programming language.

---
+++ ADD.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the value of a: \n");
+scanf("%d",&a);
+printf("Enter the value of b: \n");
+scanf("%d",&b);
+c=a+b;
+printf("The value of c is: %d",c);
+getch();
+}

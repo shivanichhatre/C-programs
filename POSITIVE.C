850a49f827438a40e3080d468950fb65d7e90074
Shivani Chhatre <69643579+shivanichhatre@users.noreply.github.com>
2020-08-15 19:44:39 +0530

    Add files via upload
    check the number is positive or negative

---
+++ POSITIVE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("Enter the value of a to be checked: ");
+scanf("%d",&a);
+if(a>=0)
+printf("The value is a positive");
+else
+printf("The value is negative ");
+getch();
+}

ac5c6665546686224dcd3e3333e178a0fd9ad3f1
Shivani Chhatre <69643579+shivanichhatre@users.noreply.github.com>
2020-08-16 15:00:30 +0530

    c
    check number is even or odd

---
+++ EVEN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter the value of a is to be checked: ");
+scanf("%d",&a);
+if(a==2)
+printf("the value is even");
+else
+printf("the value is odd");
+getch();
+}
+
+

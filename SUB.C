03be1d53e0b4248d7e01a590296c90f27d79f797
Shivani Chhatre <69643579+shivanichhatre@users.noreply.github.com>
2020-08-20 14:47:35 +0530

    c
    here is subtraction of two integers program
    we have taken three variables x,y and z
    we declared value of x and y then put the formula of subtraction is z=x+y
    then this numbers are subtract with (-)operator and  the result is stored into z 
    now finally the the printf() function is used to display the sum of integers (x-y=z)

---
+++ SUB.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y,z;
+clrscr();
+printf("Enter the value of x: \n");
+scanf("%d",&x);
+printf("Enter the value of y: \n");
+scanf("%d",&y);
+z=x-y;
+printf("The value of z is: ",z);
+
+getch();
+
+}

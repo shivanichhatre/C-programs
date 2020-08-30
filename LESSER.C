a5988d04ecce4cb5968a7e2c0af63cc487b90564
Shivani Chhatre <69643579+shivanichhatre@users.noreply.github.com>
2020-08-30 15:07:48 +0530

    lesser than
    basic program of lesser than between two numbers 
    in this program we have taken two variables then declared values in it after that we applied if else condition then  check which number is lesser number

---
+++ LESSER.C
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("Enter the value of a and b : ");
+scanf("%d%d",&a,&b);
+if(a>b)
+printf(" %d is greater than b",a);
+else
+printf(" %d is lesser than a",b);
+getch();
+}
+
